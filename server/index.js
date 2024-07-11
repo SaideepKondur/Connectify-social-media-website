59a8a4881c9d875199a8f94c1345a7bd92631986
SaideepKondur <saideek@clemson.edu>
2024-07-11 17:03:30 -0500

    .env changes (going live)

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan("common"));
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN}));
 
 
 //routers
